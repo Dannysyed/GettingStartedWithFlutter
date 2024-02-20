a01706b98264105bd9cf036e25016b726de404af
Danny syed <Dannysyed20@gmail.com>
2024-02-20 00:38:46 -0500

    setting up gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Center(
-          child: Text("Hello world"),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.amberAccent, Colors.amber, Colors.black54],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text("Hello World"),
+          ),
         ),
       ),
     ),
